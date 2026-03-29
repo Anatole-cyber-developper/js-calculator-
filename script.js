08db42fdd094bd024d56f495515c6755de36da8a
Anatole-cyber-developper <anatoletrouche@icloud.com>
2026-03-29 16:12:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,22 +5,11 @@ function append(value) {
 function clearDisplay() {
     document.getElementById("display").value = "";
 }
-document.addEventListener("keydown", function(e) {
-    const key = e.key;
-    if (!isNaN(key) || "+-*/.".includes(key)) {
-        append(key);
-    } else if (key === "Enter") {
-        calculate();
-    } else if (key === "Backspace") {
-        clearDisplay();
-    }
-    <input type="text" id="display">
-});
+
 function calculate() {
     try {
-        let result = Function("return " + document.getElementById("display").value)();
-        let display = document.getElementById("display");
-        display.value = result;
+        document.getElementById("display").value =
+            eval(document.getElementById("display").value);
     } catch {
         alert("Erreur");
     }
